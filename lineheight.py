7da4553d39faef9aaf4aea71f4f9dca1e05f234c
Ricardo Pereira <contato@ricardopdias.com.br>
2020-06-02 10:47:23 -0300

    Ajustes e criação do gitignore

--- lineheight.py
+++ lineheight.py
@@ -5,17 +5,24 @@
 from gi.repository import GObject, Gtk, Gedit
 
 class LineSpacing(GObject.Object, Gedit.ViewActivatable):
-    __gtype_name__ = "LineSpacing"
-    view = GObject.property(type=Gedit.View)
-    pixels = 5 # <<-- mude para ajustar as entrelinhas (o padrão é 0)
+	__gtype_name__ = "LineSpacing"
+	view = GObject.property(type=Gedit.View)
+	pixels = 5 # <<-- mude para ajustar as entrelinhas (o padrão é 0)
 
-    def __init__(self):
-        GObject.Object.__init__(self)
+	def __init__(self):
+		GObject.Object.__init__(self)
 
-    def do_activate(self):
-        if self.view:
-            self.view.set_pixels_below_lines(self.pixels)
+	def do_activate(self):
+		if self.view:
+			self.view.set_pixels_below_lines(self.pixels)
 
-    def do_deactivate(self):
-        if self.view:
-            self.view.set_pixels_below_lines(0)
+
+	def do_deactivate(self):
+		if self.view:
+			self.view.set_pixels_below_lines(0)
+
+
+
+
+        
+        
